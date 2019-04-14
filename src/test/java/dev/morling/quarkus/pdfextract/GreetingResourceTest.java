3ebbd2c52c9d7b21012a5bc53128ce6e64340829
Gunnar Morling <gunnar.morling@googlemail.com>
2019-04-14 15:13:05 +0200

    Disabling test for now

--- src/test/java/dev/morling/quarkus/pdfextract/GreetingResourceTest.java
+++ src/test/java/dev/morling/quarkus/pdfextract/GreetingResourceTest.java
@@ -1,21 +1,16 @@
 package dev.morling.quarkus.pdfextract;
 
 import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
 public class GreetingResourceTest {
 
-    @Test
-    public void testHelloEndpoint() {
-        given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
-    }
-
+//    @Test
+//    public void testHelloEndpoint() {
+//        given()
+//          .when().get("/hello")
+//          .then()
+//             .statusCode(200)
+//             .body(is("hello"));
+//    }
 }
